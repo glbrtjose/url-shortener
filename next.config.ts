e763e7df412d5ea98c7d4e77a460d56f1b685c8a
glbrtjose <gilbertocarrizo@gmail.com>
2024-11-23 18:35:53 -0400

    Remove Content-Security-Policy header from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,5 @@
 import type { NextConfig } from "next";
 
-const cspHeader = `
-    default-src 'self' https://*.amazon.com ${process.env.NEXT_PUBLIC_API_URL} ${process.env.NEXT_PUBLIC_API_URL}/api/urls https://vercel.com;
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.amazon.com https://vercel.com https://*.vercel.com;
-    style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://fonts.googleapis.com;
-    img-src 'self' data:  https://*.amazon.com https://vercel.com https://*.vercel.com;
-    font-src 'self' https://*.amazon.com https://vercel.com https://*.vercel.com;
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self' https://*.amazon.com https://vercel.com https://*.vercel.com ${process.env.NEXT_PUBLIC_API_URL};
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-`;
-
 const nextConfig: NextConfig = {
   /* config options here */
   async headers() {
@@ -31,10 +18,6 @@ const nextConfig: NextConfig = {
           {
             key: "X-XSS-Protection",
             value: "1; mode=block",
-          },
-          {
-            key: "Content-Security-Policy",
-            value: cspHeader.replace(/\n/g, ""),
           },
         ],
       },
